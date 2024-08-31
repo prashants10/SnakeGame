02bb53992e212d4aee8a04ad9767e5a9c329681f
Prashant Sharma <prashant.sharma@thoughts2binary.com>
2024-08-31 18:16:47 +0530

    Added Snake and food draw logic

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
   const gameArena = document.getElementById("game-arena");
+  const arenaSize = 600;
+  const cellSize = 20;
+  let score = 0;
+  let food = { x: 300, y: 200 }; // {x: 15*20, y: 10*20} // -> cell coordinate -> pixels// top left pixels for food
+  let snake = [
+    { x: 160, y: 200 },
+    { x: 140, y: 200 },
+    { x: 120, y: 200 },
+  ]; // [head, body, body, tail]
+
+  function drawDiv(cell, className) {
+    const div = document.createElement("div");
+    div.classList.add(className);
+    div.style.top = `${cell.y}px`;
+    div.style.left = `${cell.x}px`;
+    return div;
+  }
+
+  function drawSnakeAndFood() {
+    gameArena.innerHTML = "";
+    snake.forEach((snakeCell) => {
+      const snakeElement = drawDiv(snakeCell, "snake");
+      gameArena.appendChild(snakeElement);
+    });
+
+    const foodElement = drawDiv(food, "food");
+    gameArena.appendChild(foodElement);
+  }
+
+  function runGame() {
+    drawSnakeAndFood();
+  }
 
   function initiateGame() {
     const scoreBoard = document.createElement("div");
@@ -12,6 +44,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     startButton.addEventListener("click", function () {
       startButton.style.display = "none";
+      runGame();
     });
 
     document.body.appendChild(startButton);
