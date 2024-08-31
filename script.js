f96e8a6815dd83aea5f7fd2b76c8671724b87e17
Prashant Sharma <prashant.sharma@thoughts2binary.com>
2024-08-31 18:46:14 +0530

    Completed Snake Game

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ document.addEventListener("DOMContentLoaded", function () {
     { x: 120, y: 200 },
   ]; // [head, body, body, tail]
 
+  let dx = 20;
+  let dy = 0;
+  let intervalId;
+  let gameSpeed = 100;
+
   function drawDiv(cell, className) {
     const div = document.createElement("div");
     div.classList.add(className);
@@ -29,8 +34,106 @@ document.addEventListener("DOMContentLoaded", function () {
     gameArena.appendChild(foodElement);
   }
 
+  function updateSnake() {
+    const newHead = { x: snake[0].x + dx, y: snake[0].y + dy };
+    snake.unshift(newHead);
+
+    if (newHead.x === food.x && newHead.y === food.y) {
+      score += 10;
+      document.getElementById("score-board").textContent = `Score: ${score}`;
+      if (gameSpeed > 50) {
+        clearInterval(intervalId);
+        gameSpeed -= 10;
+        gameLoop();
+      }
+      moveFood();
+    } else {
+      snake.pop();
+    }
+  }
+
+  function moveFood() {
+    let newX, newY;
+
+    do {
+      newX = Math.floor(Math.random() * 30) * cellSize;
+      newY = Math.floor(Math.random() * 30) * cellSize;
+    } while (snake.some((snakeCell) => snakeCell.x === newX && snakeCell.y === newY));
+
+    food = { x: newX, y: newY };
+  }
+
+  function changeDirection(e) {
+    const isGoingDown = dy === cellSize;
+    const isGoingUp = dy === -cellSize;
+    const isGoingRight = dx === cellSize;
+    const isGoingLeft = dx === -cellSize;
+
+    if (e.key === "ArrowUp" && !isGoingDown) {
+      dx = 0;
+      dy = -cellSize;
+    } else if (e.key === "ArrowDown" && !isGoingUp) {
+      dx = 0;
+      dy = cellSize;
+    } else if (e.key === "ArrowLeft" && !isGoingRight) {
+      dx = -cellSize;
+      dy = 0;
+    } else if (e.key === "ArrowRight" && !isGoingLeft) {
+      dx = cellSize;
+      dy = 0;
+    }
+  }
+
+  function isGameOver() {
+    // snake collision checks
+    for (let i = 1; i < snake.length; i++) {
+      if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+        return true;
+      }
+    }
+
+    // wall collision checks
+    const hitLeftWall = snake[0].x < 0; // snake[0] -> head
+    const hitRightWall = snake[0].x > arenaSize - cellSize;
+    const hitTopWall = snake[0].y < 0;
+    const hitBottomWall = snake[0].y > arenaSize - cellSize;
+    return hitLeftWall || hitRightWall || hitTopWall || hitBottomWall;
+  }
+
+  function restartGame() {
+    score = 0;
+    food = { x: 300, y: 200 }; // {x: 15*20, y: 10*20} // -> cell coordinate -> pixels// top left pixels for food
+    snake = [
+      { x: 160, y: 200 },
+      { x: 140, y: 200 },
+      { x: 120, y: 200 },
+    ]; // [head, body, body, tail]
+
+    dx = 20;
+    dy = 0;
+    gameSpeed = 100;
+    gameLoop();
+  }
+
+  function gameLoop() {
+    intervalId = setInterval(() => {
+      if (isGameOver()) {
+        clearInterval(intervalId);
+        gameStarted = false;
+        alert("Game Over" + "\n" + "Your Score: " + score + "\nRestart Game");
+        restartGame();
+        return;
+      }
+      updateSnake();
+      drawSnakeAndFood();
+      drawScoreBoard();
+    }, gameSpeed);
+  }
+
   function runGame() {
-    drawSnakeAndFood();
+    // drawSnakeAndFood();
+    document.addEventListener("keydown", changeDirection);
+    gameLoop();
   }
 
   function initiateGame() {
